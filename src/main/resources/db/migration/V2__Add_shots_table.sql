02eb024ba248a69988121042141051b0ce369013
D McLarnon <dmclarnon@Ds-Mac-mini.local>
2025-09-06 20:15:36 +0100

    modified shots to include distance

--- src/main/resources/db/migration/V2__Add_shots_table.sql
+++ src/main/resources/db/migration/V2__Add_shots_table.sql
@@ -1,7 +1,8 @@
 CREATE TABLE shots (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     shot_number INT NOT NULL,
     club VARCHAR(20) NOT NULL,
     hole_score_id BIGINT NOT NULL,
+    distance_yards INTEGER,
     CONSTRAINT fk_shot_hole_score FOREIGN KEY (hole_score_id) REFERENCES hole_score(id) ON DELETE CASCADE
 );
